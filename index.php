c4e8c43bd6388af421e4cd23e73a77af11ecd193
DEV10PEDRO <adielson-pedro@outlook.com>
2024-07-10 17:18:32 -0300

    exercicio

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// Q1. Crie uma variável em PHP, atribua um valor e exiba na página.
+
+$mensagem = "Olá, pedro";
+echo $mensagem;
+
+// Q2. Exiba na tela as variáveis nome, idade, endereco e curso fazendo a concatenação dos valores dentro do seguinte texto.
+
+$nome = "pedro";
+$idade = "30";
+$endereco = "av.";
+$curso = "senac";
+echo "Ola eu sou: "$nome . 
+
+
+
+
+
+// Q3.
+?>
